c92860ea2ca76966391c07bd915346df627dcc85
ramon05 <ramoncastillo051991@gmail.com>
2018-06-30 16:31:01 -0400

    se creo un activity DescripcionAnimeActivity para la descripcion de los animes

---
+++ app/src/main/java/com/example/ramon/animelist/DescripcionAnimeActivity.java
@@ -0,0 +1,13 @@
+package com.example.ramon.animelist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DescripcionAnimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_descripcion_anime);
+    }
+}
